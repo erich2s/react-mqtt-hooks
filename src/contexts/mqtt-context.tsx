d9b5a214520243e1ca04400a8e1627b14a51a4ba
Eric Huang <erich2s0103@gmail.com>
2025-02-04 00:21:06 +0800

    fix

--- src/contexts/mqtt-context.tsx
+++ src/contexts/mqtt-context.tsx
@@ -1,11 +1,8 @@
 import type { MqttClient } from "mqtt";
 import { createContext } from "react";
 
-// export type MqttClientType = {
-//   client: MqttClient | null;
-//   status: typeof MQTT_STATUS[keyof typeof MQTT_STATUS];
-// };
 export type MqttClientType = MqttClient | null;
+
 export const MQTT_STATUS = {
   CONNECTING: "connecting",
   DISCONNECTED: "disconnected",
